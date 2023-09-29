23da6d6a1eb0399c36fa86800c0c448d4a9bf789
Mohammed Parves <mdperrfan@gmail.com>
2023-09-29 20:30:01 +0600

    updated code

--- srtf.c
+++ srtf.c
@@ -1,5 +1,33 @@
 #include<stdio.h>
 int main(){
-    printf("hello world!");
+    int at[10],bt[10],rt[10],ct[10],CT,i,smallest,n,remain=0,time=0,ttat=0,twt=0;
+    printf("Enter number of processes:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("\nEnter arrival time for process %d\n",i+1);
+        scanf("%d",&at[i]);
+        printf("Enter burst time for process %d\n",i+1);
+        scanf("%d",&bt[i]);
+        rt[i]=bt[i];
+    }
+    printf("\nProcess\t|Turnaround Time|Waiting Time\n");
+    for(time=0;remain!=n;time++){
+        for(i=0;i<n;i++){
+            if(at[i]<=time && rt[i]){
+                smallest=i;
+            }
+        }
+        rt[smallest]--;
+        if(rt[smallest]==0){
+            remain++;
+            CT=time+1;
+            printf("\nP%d\t|\t%d\t|\t%d\t",smallest+1,CT-at[smallest],CT-at[smallest]-bt[smallest]);
+            twt+=CT-at[smallest]-bt[smallest];
+            ttat+=CT-at[smallest];
+        }
+    }
+    printf("\nAverage Waiting Time : %f",twt*1.0/n);
+    printf("\nAverage Turnaround Time : %f",ttat*1.0/n);
     return 0;
 }
+
